21c2a1ea8762eb1fb7dc7e01297076f30cdf389f
Jakub Nicewicz <njakub@Jakubs-MacBook-Pro.local>
2024-03-01 11:58:32 +0000

    changing to postgress

--- prisma/migrations/20240225232542_initial_migration/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `Rating` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `comment` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
